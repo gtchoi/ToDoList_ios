4ddb63f90372160a4f9d79a56fec2944fd62c1d1
최규태 <gtchoi@nhnent.com>
2015-09-14 18:47:46 +0900

    Tutorials : view static cell.

---
+++ ToDoList/AddToDoItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddToDoItemViewController.h
+//  ToDoList
+//
+//  Created by 최규태 on 9/13/15.
+//  Copyright (c) 2015 최규태. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddToDoItemViewController : UIViewController
+
+@end
---
+++ ToDoList/TodoListTableTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  TodoListTableTableViewController.h
+//  ToDoList
+//
+//  Created by 최규태 on 9/13/15.
+//  Copyright (c) 2015 최규태. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodoListTableTableViewController : UITableViewController
+
+- (IBAction)unwindToList:(UIStoryboardSegue*)segue;
+
+@end
